46386d3599a3f6e05c4adfd8302781d670d3fbcd
최정혁 <jeonghyeogc01@gmaile.com>
2025-10-20 12:13:41 +0900

    Multiply2 두 수의 나눗셈

---
+++ src/level1/Multiply2.java
@@ -0,0 +1,10 @@
+package level1;
+
+public class Multiply2 {
+    public int multiply(int num1, int num2) {
+        double result = 0.0;
+        result = ((double) num1 / num2) * 1000;
+        return (int) result;
+
+    }
+}
