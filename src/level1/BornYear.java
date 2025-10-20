a5e86ad95496917ee7a7efd70ba6a2d857fd37ca
최정혁 <jeonghyeogc01@gmaile.com>
2025-10-20 11:39:02 +0900

    BornYear 출생일자 구하기

---
+++ src/level1/BornYear.java
@@ -0,0 +1,10 @@
+package level1;
+
+public class BornYear {
+        public int solution(int age) {
+            int bornYear = 0;
+            bornYear = 2022 - (age - 1);
+            return bornYear;
+        }
+
+}
