3734500475dba18419f66b6e3affd56da973f63e
최정혁 <jeonghyeogc01@gmaile.com>
2025-10-20 11:05:59 +0900

    Subtract 두 수의 차 구하기

---
+++ src/level1/Subtract.java
@@ -0,0 +1,10 @@
+package level1;
+
+public class Subtract {
+    public int subtract(int num1, int num2) {
+        // 두 수의 차 구하기
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
+    }
+}
