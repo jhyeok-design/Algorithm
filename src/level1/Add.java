21b0d8ff85d4c0a5dc8427f3eed238cfdea6fec2
최정혁 <jeonghyeogc01@gmaile.com>
2025-10-20 11:53:25 +0900

    Add 두 수의 합 구하기

---
+++ src/level1/Add.java
@@ -0,0 +1,10 @@
+package level1;
+
+public class Add {
+        public int solution(int num1, int num2) {
+            int result = 0;
+            result = num1 + num2;
+            return result;
+        }
+    }
+
