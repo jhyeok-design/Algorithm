652b4a85f012a764ff4b7ca49a11964909244d6f
최정혁 <jeonghyeogc01@gmaile.com>
2025-10-20 14:47:27 +0900

    EvenNumber 짝수의 합 구하기

---
+++ src/level1/EvenNumber.java
@@ -0,0 +1,14 @@
+package level1;
+
+public class EvenNumber {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            if (i % 2 == 0) {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+}
