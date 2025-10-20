8412371e9f1b59409121fbb8da8b88b8e7b710ae
최정혁 <jeonghyeogc01@gmaile.com>
2025-10-20 11:17:14 +0900

    Multiply 두 수의 곱 구하기

---
+++ src/level1/Multiply.java
@@ -0,0 +1,10 @@
+package level1;
+
+public class Multiply {
+
+    public int multiply(int num1, int num2) {
+        int answer = 0;
+        answer = num1 * num2;
+        return answer;
+    }
+}
