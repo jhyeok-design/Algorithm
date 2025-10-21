2b178fdee8e8aa7e1783652bf716a27ee68f841e
jhyeok-design <jeonghyeogc1@gmail.com>
2025-10-21 16:57:57 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 90.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(int x) {
+        int num = x;  
+        int sum = 0;
+        
+        while (x > 0) {
+            sum += x % 10; 
+            x /= 10;
+        }
+        return num % sum == 0;
+    }
+}
